8e292e37b34d50f3cf62eaa07477e19407b27b57
613S038\sse <1610576559@qq.com>
2017-02-15 17:44:38 +0800

    Finish HomeWork1

---
+++ out/main.js
@@ -0,0 +1,77 @@
+window.onload = function () {
+    var c = document.getElementById("myCanvas");
+    var cxt = c.getContext("2d");
+    cxt.fillText("picture2", 50, 50);
+    var image = new Image();
+    image.src = "picture2.png";
+    image.onload = function () {
+        cxt.clearRect(0, 0, c.width, c.height);
+        cxt.drawImage(image, 0, 0);
+    };
+    var stage = new DisplayObjectContainer();
+    setInterval(function () {
+        cxt.clearRect(0, 0, c.width, c.height);
+        stage.draw(cxt);
+    }, 30);
+    var img = new Bitmap();
+    img.name = "picture1.png";
+    img.x = 50;
+    img.y = 50;
+    var tf = new TextField();
+    tf.text = "picture1";
+    tf.x = 50;
+    tf.y = 50;
+    tf.textColor = "#0000FF";
+    stage.addChild(img);
+    stage.addChild(tf);
+};
+var DisplayObjectContainer = (function () {
+    function DisplayObjectContainer() {
+        this.array = [];
+    }
+    DisplayObjectContainer.prototype.draw = function (context2D) {
+        for (var _i = 0, _a = this.array; _i < _a.length; _i++) {
+            var drawable = _a[_i];
+            drawable.draw(context2D);
+        }
+    };
+    DisplayObjectContainer.prototype.addChild = function (child) {
+        this.array.push(child);
+    };
+    return DisplayObjectContainer;
+}());
+var Bitmap = (function () {
+    function Bitmap() {
+        this.x = 0;
+        this.y = 0;
+        this.width = -1;
+        this.height = -1;
+        this.name = "";
+    }
+    Bitmap.prototype.draw = function (context2D) {
+        var _this = this;
+        var image = document.createElement("img");
+        image.src = this.name;
+        context2D.drawImage(image, this.x, this.y);
+        image.onload = function () { context2D.drawImage(image, _this.x, _this.y); };
+    };
+    return Bitmap;
+}());
+var TextField = (function () {
+    function TextField() {
+        this.x = 0;
+        this.y = 0;
+        this.text = "";
+        this.textColor = "#000000";
+    }
+    TextField.prototype.draw = function (context2D) {
+        this.toggleCase();
+        context2D.fillStyle = this.textColor;
+        context2D.fillText(this.text, this.x, this.y, 100);
+    };
+    TextField.prototype.toggleCase = function () {
+        this.textColor.toLocaleUpperCase();
+    };
+    return TextField;
+}());
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,90 @@
+
+
+window.onload = () => {
+
+    var c=document.getElementById("myCanvas") as HTMLCanvasElement;
+    var cxt=c.getContext("2d");
+    cxt.fillText("picture2", 50, 50);
+    var image = new Image();
+    image.src = "picture2.png";
+    image.onload = () => {
+        cxt.clearRect(0,0,c.width,c.height);
+        cxt.drawImage(image, 0, 0);
+    } 
+    
+
+    var stage: DisplayObjectContainer = new DisplayObjectContainer();
+    setInterval(() => {
+        cxt.clearRect(0, 0, c.width, c.height);
+        stage.draw(cxt);
+    }, 30);
+
+    var img: Bitmap = new Bitmap();
+    img.name = "picture1.png"
+    img.x = 50;
+    img.y = 50;
+    
+
+    var tf: TextField = new TextField();
+    tf.text = "picture1";
+    tf.x = 50;
+    tf.y = 50;
+    tf.textColor = "#0000FF"
+   
+  
+    stage.addChild(img);
+    stage.addChild(tf);
+};
+
+interface Drawable {
+    draw(context2D: CanvasRenderingContext2D);
+}
+
+
+class DisplayObjectContainer implements Drawable {
+    array: Drawable[] = [];
+    
+    draw(context2D: CanvasRenderingContext2D) {
+        for (let drawable of this.array) {
+            drawable.draw(context2D);
+        }
+    }
+    addChild(child: Drawable) {
+        this.array.push(child);
+    }
+
+}
+
+
+
+class Bitmap implements Drawable {
+    x: number = 0;
+    y: number = 0;
+    width: number = -1;
+    height: number = -1;
+    name: string = "";
+    draw(context2D: CanvasRenderingContext2D) {
+        var image = document.createElement("img");
+        image.src = this.name;
+        context2D.drawImage(image, this.x, this.y);
+        image.onload = () => {context2D.drawImage(image, this.x, this.y);} 
+        }
+    }
+
+
+
+class TextField implements Drawable {
+    x: number = 0;
+    y: number = 0;
+    text: string = "";
+    textColor: string = "#000000"
+   draw(context2D: CanvasRenderingContext2D) {
+        this.toggleCase();
+        context2D.fillStyle = this.textColor;
+        context2D.fillText(this.text, this.x, this.y, 100);
+    }
+
+    private toggleCase() {
+        this.textColor.toLocaleUpperCase();
+    }
+}
